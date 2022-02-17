c943004ab181917cf4ea08abfe03acd691d09c40
admin <olevist@gmail.com>
2022-02-17 18:32:36 +0200

    new

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { useFonts } from 'expo-font';
+
+import Header from './components/Header';
+import GameOverScreen from './screens/GameOverScreen';
+import GameScreen from './screens/GameScreen';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
+
+
+     const configureNewGameHandler = () => {
+        setGuessRounds(0);
+        setUserNumber(null);
+      };
+    const [loaded] = useFonts({
+                    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+                    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+                    });
+    if (!loaded) {
+        return null;
+    }
+      const startGameHandler = selectedNumber => {
+        setUserNumber(selectedNumber);
+      };
+
+      const gameOverHandler = numOfRounds => {
+        setGuessRounds(numOfRounds);
+      };
+
+      let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+      if (userNumber && guessRounds <= 0) {
+        content = (
+          <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+        );
+      } else if (guessRounds > 0) {
+        content = (
+          <GameOverScreen
+            roundsNumber={guessRounds}
+            userNumber={userNumber}
+            onRestart={configureNewGameHandler}
+          />
+        );
+      }
+    return (
+    <SafeAreaView style={styles.screen}>
+          <Header title="Guess a Number" />
+          {content}
+    </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+      flex: 1
+  }
 });
